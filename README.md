15d14962ce0e929cddb6d41283c299e1ee56660a
Aaron Watters <awatters@simonsfoundation.org>
2022-09-09 14:19:36 -0400

    install stuff

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # array_gizmos
-A collection of array operations and visualizations
+A collection of array operations and visualizations.
+
+# Installation
+
+This module is in experimental/development mode and it also uses experimental dependancies.
+
+Install the H5Gizmos dependancy manually in development mode:
+
+```bash
+% git clone https://github.com/AaronWatters/H5Gizmos.git
+% cd H5Gizmos
+% pip install -e .
+% cd ..
+```
+
+Then install this module in development mode:
+
+```bash
+% git clone https://github.com/AaronWatters/array_gizmos.git
+% cd array_gizmos
+% pip install -e .
+% cd ..
+```
+
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+url = "https://github.com/AaronWatters/array_gizmos"
+version = "0.1.0"
+readme = open('README.md').read()
+
+setup(
+    name="array_gizmos",
+    packages=[
+        "array_gizmos",
+        ],
+    version=version,
+    description="A collection of array operations and visualizations",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    #include_package_data=True,
+    author="Aaron Watters",
+    author_email="awatters@flatironinstitute.org",
+    url=url,
+    install_requires=[
+        "numpy",
+        "H5Gizmos",
+        ],
+    python_requires=">=3.6",
+)
